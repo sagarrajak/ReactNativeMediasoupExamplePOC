c2494c18fbffb8d2b65ff505331e3bdbcc6aae8e
donkey king senior <sagarrajak858@gmail.com>
2025-12-26 14:33:09 +0530

    work in progress

---
+++ App.tsx
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { StatusBar, useColorScheme } from 'react-native';
+import {
+  SafeAreaProvider
+} from 'react-native-safe-area-context';
+import Main from './src/Main';
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaProvider>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <Main/>
+    </SafeAreaProvider>
+  );
+}
+
+
+export default App;
